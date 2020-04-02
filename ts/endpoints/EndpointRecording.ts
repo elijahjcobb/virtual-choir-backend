78babe6a4221a2309f16b4f1fd4a0128e94cd1ec
Elijah Cobb <elijah@elijahcobb.com>
2020-04-02 15:00:58 -0400

    changed recording endpoint

--- ts/endpoints/EndpointRecording.ts
+++ ts/endpoints/EndpointRecording.ts
@@ -6,9 +6,8 @@
  */
 
 import {HEndpointGroup, HErrorStatusCode, HRequest, HResponse} from "@element-ts/hydrogen";
-import {Recording} from "../objects/Recording";
+import {Recording, RecordingProps} from "../objects/Recording";
 import {SiQuery} from "@element-ts/silicon";
-import {Submission} from "../objects/Submission";
 import {Part, PartProps} from "../objects/Part";
 
 export const endpointRecording: HEndpointGroup = new HEndpointGroup();
@@ -18,19 +17,12 @@ export const endpointRecording: HEndpointGroup = new HEndpointGroup();
  */
 endpointRecording.getDynamic(async(req: HRequest, res: HResponse): Promise<void> => {
 
-	const submissionId: string = req.getEndpoint();
-	const submission: Submission | undefined = await SiQuery.getObjectForId(Submission, submissionId);
-	if (submission === undefined) return res.err(HErrorStatusCode.NotAcceptable, "Submission does not exist. Call 'POST /submission' first.");
-	if (submission.props.hasVerified !== true) return res.err(HErrorStatusCode.NotAcceptable, "Submission not verified, call 'POST /submission/verify' first.");
-	if (submission.props.recordingId === undefined) return res.err(HErrorStatusCode.NotAcceptable, "Submission's recordingId is undefined.");
- 	const recording: Recording | undefined = await SiQuery.getObjectForId(Recording, submission.props.recordingId);
-	if (!recording) return res.err(HErrorStatusCode.NotFound, "The recording specified could not be found.");
-
-	const query: SiQuery<Part, PartProps> = new SiQuery<Part, PartProps>(Part, {
-		recordingId: submission.props.recordingId
-	});
-
-	const parts: Part[] = await query.getAll();
+	const key: string = req.getEndpoint();
+	const recordingQuery: SiQuery<Recording, RecordingProps> = new SiQuery<Recording, RecordingProps>(Recording, {key});
+	const recording: Recording | undefined = await recordingQuery.getFirst();
+	if (recording === undefined) return res.err(HErrorStatusCode.NotFound, "Recording does not exist.");
+	const partsQuery: SiQuery<Part, PartProps> = new SiQuery<Part, PartProps>(Part, {recordingId: recording.getId()});
+	const parts: Part[] = await partsQuery.getAll();
 	const formattedParts: object[] = [];
 	for (const part of parts) formattedParts.push(part.bond());
 
